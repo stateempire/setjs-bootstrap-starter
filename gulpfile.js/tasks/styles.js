3643363814568e35bd711720e47e9da0480038d8
kashifimran <kashifimran2008@gmail.com>
2021-03-09 13:05:53 +0500

    Initial sass/scss built with bootstrap

---
+++ gulpfile.js/tasks/styles.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
+var gulpif = require('gulp-if');
+var cleanCSS = require('gulp-clean-css');
+var plumber = require('gulp-plumber');
+var browserSync = require('browser-sync');
+var {paths, env} = require('../setup.js');
+
+sass.compiler = require('node-sass');
+
+function buildCss(path) {
+  return function () {
+    return gulp.src(`${paths.src.styles}/${path}.scss`)
+      .pipe(plumber())
+      .pipe(postcss([require('stylelint')], {syntax: require('postcss-scss')}))
+      .pipe(sass().on('error', sass.logError))
+      .pipe(postcss([require('autoprefixer')]))
+      .pipe(gulpif(env.current.mode != env.local.mode, cleanCSS()))
+      .pipe(gulp.dest(paths.dest.styles))
+      .pipe(gulpif(env.current.mode == env.local.mode, browserSync.get('setjs-serve').stream()));
+  };
+}
+
+exports.styles = gulp.parallel(
+  // buildCss('common'),
+  buildCss('main'),
+);
